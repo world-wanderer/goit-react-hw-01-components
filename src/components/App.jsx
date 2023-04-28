f51a48726568096cec0188d4c820d46e19a5ef24
Kateryna Vynnyk <ukraina.666@ukr.net>
2023-04-28 19:17:53 +0300

    corretc name Data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,10 @@ import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
 import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 
-import user from './Data/user.json';
-import data from './Data/data.json';
-import friends from './Data/friends.json';
-import transactions from './Data/transactions.json';
+import user from './data/user.json';
+import data from './data/data.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 
 export const App = () => {
   return (
